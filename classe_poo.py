ba6a826676b145ab643544ae6c7c25c8afe5a5f1
tanguy-goepfert <t.goepfert@orange.fr>
2026-04-21 16:19:29 +0200

    classe

--- classe_poo.py
+++ classe_poo.py
@@ -1,3 +1,5 @@
+
+
 class Case:
     def __init__(self, disponibilite, couleur):
         self.disponibilite = disponibilite
@@ -19,8 +21,30 @@ class Grille :
     def __init__(self, tableau):
         self.tableau = tableau
         
+class Partie :
+    def __init__(self, id_partie, tour, largeur_grille, longueur_grille, nombre_case_bloquee):
+        self.id_partie = id_partie
+        self.tour = tour
+        self.largeur_grille = largeur_grille
+        self.longueur_grille = longueur_grille
+        self.nombre_case_bloquee = nombre_case_bloquee
     
+    # Methodes
+    def jouer(param):
+        pass
 
+class Figure():
+    def __init__(self, nom, description, nombre_de_point, etat):
+        self.nom = nom                              # 'O', 'I', 'T' par ex
+        self.descritpion = descritpion              # toute une colonne/ligne remplie
+        self.nombre_de_point = nombre_de_point      # Nombre de points si cette figure est réalisée. 
+        self.score = 0                              # Initialisation du score
+    
+    # Methodes
+    def est_respectee(): #nico est dessus
+        pass
+        
+        
 if __name__ == "__main__":
     case1 = Case(True, "green")
     
