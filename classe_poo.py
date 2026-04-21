fea2c699e2f513ec49350dc2c866d4f67c8856e1
tanguy-goepfert <t.goepfert@orange.fr>
2026-04-21 14:12:11 +0200

    classe

--- classe_poo.py
+++ classe_poo.py
@@ -2,11 +2,23 @@ class Case:
     def __init__(self, disponibilite, couleur):
         self.disponibilite = disponibilite
         self.couleur = couleur
-        
 
 class Polymino(Case) :
     def __init__(self, occupation, couleur, forme):
         self.forme = forme
         self.couleur = couleur
         
-class Case_bloque : 
+class Case_bloque(self, disponibilite, couleur) : 
+    def __init__(self, )
+
+class Grille :
+    def __init__(self, tableau):
+        self.tableau = tableau
+        
+    
+
+if __name__ == "__main__":
+    case1 = Case(True, "green")
+    
+    
+    
