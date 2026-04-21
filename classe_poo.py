53906d3af5ae4d56aaef294c4a3667bfda83605d
damien-labataille <damienlabataille@gmail.com>
2026-04-21 16:24:48 +0200

    test

--- classe_poo.py
+++ classe_poo.py
@@ -1,3 +1,7 @@
+import TP_CARTO
+
+
+
 class Case:
     def __init__(self, disponibilite, couleur):
         self.disponibilite = disponibilite
@@ -8,17 +12,26 @@ class Polymino(Case) :
         self.forme = forme
         self.couleur = couleur
         
-class Case_bloque(self, disponibilite, couleur) : 
-    def __init__(self, )
+# class Case_bloque(self, disponibilite, couleur) : 
+#     def __init__(self, )
 
 class Grille :
     def __init__(self, tableau):
         self.tableau = tableau
         
+        
+    def integration(self,poly,case_depart):
+        
+        for i in range(len(self.tableau)):
+            for j in range(len(self.tableau[i])):
+                poly[]
+        
+        
+        
     
 
-if __name__ == "__main__":
-    case1 = Case(True, "green")
+# if __name__ == "__main__":
+#     case1 = Case(True, "green")
     
     
     
